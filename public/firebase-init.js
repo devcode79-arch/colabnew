48ce52ea7bd5ad86d4b440725d30b651135520ed
devcode79-arch <devcode79@gmail.com>
2026-01-08 12:37:15 +0530

    Change Firebase config for new project
    Updated Firebase configuration with new project details.

--- public/firebase-init.js
+++ public/firebase-init.js
@@ -2,14 +2,15 @@
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDmrjFbHD_h93pastwaDwtVvNyX27EiaIU",
-  authDomain: "colab-blog.firebaseapp.com",
-  projectId: "colab-blog",
-  storageBucket: "colab-blog.firebasestorage.app",
-  messagingSenderId: "943366440260",
-  appId: "1:943366440260:web:6ec4473cf983962897c311",
-  measurementId: "G-2PQ00WE24B"
+  apiKey: "AIzaSyDHyQvjjHK5L7M_eeRzCIl8KnSFWBZUyyE",
+  authDomain: "colabnew-8d474.firebaseapp.com",
+  projectId: "colabnew-8d474",
+  storageBucket: "colabnew-8d474.firebasestorage.app",
+  messagingSenderId: "1061842493012",
+  appId: "1:1061842493012:web:551e3fd12d228ae4b6d7c1",
+  measurementId: "G-42MXHKXMMQ"
 };
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
